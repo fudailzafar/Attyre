5e24ef8eff7e8c3a738e27dc45a7d9b5798cd4b5
Fudail Mohammed Zafar <142711828+fudailzafar@users.noreply.github.com>
2025-06-26 20:50:09 +0530

    fix: responsive design for personalised page

--- src/components/ColourSection.jsx
+++ src/components/ColourSection.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ColourSection = () => {
   return (
-    <div className="w-[100%] flex flex-row max-w-96 gap-10 py-5">
-      <div>
-        <div className="px-[90px] py-[100px] bg-[#228B22]"></div>
+    <div className="w-[100%] flex flex-row gap-3 md:gap-10 py-5">
+      <div className="w-full">
+        <div className="px-[50px] md:px-[90px] py-[70px] md:py-[100px] bg-[#228B22]"></div>
         <div>
           <h2 className="inter font-semibold text-lg py-1">Forest Green</h2>
           <h3 className="inter font-medium text-sm">Main</h3>
@@ -13,8 +13,8 @@ const ColourSection = () => {
           </p>
         </div>
       </div>
-      <div>
-        <div className="px-[90px] py-[100px] bg-[#B7410E]"></div>
+      <div className="w-full">
+        <div className="px-[50px] md:px-[90px] py-[70px] md:py-[100px] bg-[#B7410E]"></div>
         <div>
           <h2 className="inter font-semibold text-lg py-1">Rust</h2>
           <h3 className="inter font-medium text-sm">Main</h3>
@@ -24,7 +24,7 @@ const ColourSection = () => {
           </p>
         </div>
       </div>
-      <div>
+      <div className="hidden md:flex md:flex-col">
         <div className="px-[90px] py-[100px] bg-[#CC5500]"></div>
         <div>
           <h2 className="inter font-semibold text-lg py-1">Burnt Orange</h2>
@@ -34,7 +34,7 @@ const ColourSection = () => {
           </p>
         </div>
       </div>
-      <div>
+      <div className="hidden md:flex md:flex-col">
         <div className="px-[90px] py-[100px] bg-[#6B8E23]"></div>
         <div>
           <h2 className="inter font-semibold text-lg py-1">Golden Olive</h2>
@@ -44,7 +44,7 @@ const ColourSection = () => {
           </p>
         </div>
       </div>
-      <div>
+      <div className="hidden md:flex md:flex-col">
         <div className="px-[90px] py-[100px] bg-[#FFDB58]"></div>
         <div>
           <h2 className="inter font-semibold text-lg py-1">Mustard Yellow</h2>
--- src/components/Personalised.jsx
+++ src/components/Personalised.jsx
@@ -4,7 +4,7 @@ import ColourSection from "./ColourSection";
 const Personalised = () => {
   return (
     <div className="bg-[#F8F7F4] mx-auto">
-      <div className="flex flex-col w-[80%] md:w-[70%] justify-center items-start mx-auto">
+      <div className="flex flex-col w-[85%] md:w-[70%] justify-center items-start mx-auto">
         <div>
           <h1 className="playfair font-semibold text-4xl text-[#0C111D] mt-5">
             Your Personalized Palette
@@ -14,7 +14,7 @@ const Personalised = () => {
           <h2 className="inter font-semibold text-4xl text-[#F63D68] mt-6">
             Autumn Harmony
           </h2>
-          <p className="inter font-normal text-sm text-[#475467] mt-4">
+          <p className="inter font-normal text-[16px] md:text-sm text-[#475467] mt-4">
             A warm, earthy palette that complements the skin's warm undertones,
             emphasizes the depth of darker hair, and enhances the richness of
             brown eyes. This palette is perfect for a classic yet modern look,
